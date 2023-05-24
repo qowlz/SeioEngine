fcbc723944a53951bcfda13beae082c8902d794a
byunguk.choi <qowlz45@gmail.com>
2023-05-24 21:47:28 +0900

    Reorganize file structure

--- src/vendor/glad/glad.c
+++ src/vendor/glad/glad.c
@@ -7,7 +7,7 @@
     APIs: gl=3.3
     Profile: compatibility
     Extensions:
-        
+
     Loader: True
     Local files: False
     Omit khrplatform: False
@@ -22,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <glad/glad.h>
+#include "glad.h"
 
 static void* get_proc(const char *namez);
 
--- src/vendor/glad/glad.h
+++ src/vendor/glad/glad.h
@@ -7,7 +7,7 @@
     APIs: gl=3.3
     Profile: compatibility
     Extensions:
-        
+
     Loader: True
     Local files: False
     Omit khrplatform: False
@@ -86,7 +86,7 @@ GLAPI int gladLoadGL(void);
 
 GLAPI int gladLoadGLLoader(GLADloadproc);
 
-#include <KHR/khrplatform.h>
+#include "../KHR/khrplatform.h"
 typedef unsigned int GLenum;
 typedef unsigned char GLboolean;
 typedef unsigned int GLbitfield;
